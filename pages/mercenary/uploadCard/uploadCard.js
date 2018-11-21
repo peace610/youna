6f9cb36eaf7f0ec5d80df0b05258c41de384d54f
hepp <835606465@qq.com>
2018-11-21 15:42:09 +0800

    缴纳押金

--- pages/mercenary/uploadCard/uploadCard.js
+++ pages/mercenary/uploadCard/uploadCard.js
@@ -4,7 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      tempFilePaths: ''
   },
 
   /**
@@ -12,5 +12,37 @@ Page({
    */
   onLoad: function (options) {
 
-  }
+  },
+    uploadCard: function () {
+      const vm = this
+        wx.chooseImage({
+            count: 1,
+            // sizeType: ['original', 'compressed'],
+            // sourceType: ['album', 'camera'],
+            success (res) {
+              console.info(res)
+                const tempFilePaths = res.tempFilePaths
+                vm.setData({
+                    tempFilePaths: tempFilePaths.join(','),
+                })
+                // wx.uploadFile({
+                //     url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+                //     filePath: tempFilePaths[0],
+                //     name: 'file',
+                //     formData: {
+                //         'user': 'test'
+                //     },
+                //     success (res){
+                //         const data = res.data
+                //         //do something
+                //     }
+                // })
+            }
+        })
+    },
+    submitOrder: function () {
+        wx.redirectTo({
+            url: '/pages/mercenary/cash/cash'
+        })
+    }
 })
